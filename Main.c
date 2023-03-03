78fc268bea8f950de65338d45dd8e18254f683fc
finnthejellyisfin <finnamon.bs@gmail.com>
2023-03-03 17:45:35 -0500

    Started to impliment logo reading and print WIP almost working

--- Main.c
+++ Main.c
@@ -1,19 +1,28 @@
 //imports libraies
 #include <stdio.h> 
 #include <ncurses.h>
+#include <stdlib.h>  //relates to string.h
+#include <string.h>  // adds support for string type variables
+
 
 // Marks anything between the the next curly brackest as the main function
 int main() {
   //declaring vars
-  int run;    //sets "run" as a variable, this will be used later in the program to tell the program to move on
-
+  FILE* logo; // Makes logo a pointer
+  int run;    // sets "run" as a variable, this will be used later in the program to tell the program to move on
+  logo = fopen("log.ass", "r");  // Opens the logo file and puts it into ram as "log"
+  char line; //Makes variable that will store each line of file read from
   //setting up ncurses
   initscr();  //Creates stdscr *Used by ncurses for keyboard input*
   raw();  //defines ncurses mode, raw should be used for now
 
   //Displays opening screen and prompts the user to press enter to start the program
   //While using ncurses like we are now use "printw" instead of "printf" to display text
-  printw("never gonna give you up\n");  // when you run this code this line prints x I did this to practice the printf in the future this will print the logo
+  do { //prints logo to screen 
+  line = fgetc(logo); // Saves first line of log in pre-allocated memory
+  printw("%c", line); // Prints the var to the screen
+} while (logo != EOF);
+
   getch();  //Waits for user to press any key but soon I will make it "Enter" key specific"
 
   endwin(); //kills ncurses. If this command is not called the program will not close right, I will put it above return for now 
