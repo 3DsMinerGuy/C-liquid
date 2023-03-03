82fe4fd925fb79cba25a7d2067ae8f41f9eab894
finnthejellyisfin <finnamon.bs@gmail.com>
2023-03-02 19:51:42 -0500

    Tested and setup printf

--- Main.c
+++ Main.c
@@ -3,6 +3,7 @@
 // Marks anything between the the next curly brackest as the main function
 int main() {
   int run = 0;    //sets "run" a variable, this will be used later in the program to tell the program to move on
+  printf("never gonna give you up\n");  // when you run this code this line prints x I did this to practice the printf in the future this will print the logo
   return 0;
 
 
