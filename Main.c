a0c97b5b20c9a70ab8754f2da6dbd6b62833f35c
3DsMinerGuy <bearski.bs@gmail.com>
2023-03-02 23:28:14 -0500

    I set up the use of the ncurses library and soon it will be used to get past the opening screen. Use printw instead of printf when ncurses is in use

--- Main.c
+++ Main.c
@@ -1,9 +1,22 @@
-#include <stdio.h>  //imports libraies 
+//imports libraies
+#include <stdio.h> 
+#include <ncurses.h>
 
 // Marks anything between the the next curly brackest as the main function
 int main() {
-  int run = 0;    //sets "run" a variable, this will be used later in the program to tell the program to move on
-  printf("never gonna give you up\n");  // when you run this code this line prints x I did this to practice the printf in the future this will print the logo
+  //declaring vars
+  int run;    //sets "run" as a variable, this will be used later in the program to tell the program to move on
+
+  //setting up ncurses
+  initscr();  //Creates stdscr *Used by ncurses for keyboard input*
+  raw();  //defines ncurses mode, raw should be used for now
+
+  //Displays opening screen and prompts the user to press enter to start the program
+  //While using ncurses like we are now use "printw" instead of "printf" to display text
+  printw("never gonna give you up\n");  // when you run this code this line prints x I did this to practice the printf in the future this will print the logo
+  getch();  //Waits for user to press any key but soon I will make it "Enter" key specific"
+
+  endwin(); //kills ncurses. If this command is not called the program will not close right, I will put it above return for now 
   return 0;
 
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # C-liquid
 A terminal based application that simulates the phyisics of Ferrofluid in a colorful way 
+
+******ONLY WORKS ON LINUX RIGHT NOW WINDOWS PORT MAY COME SOON*****
+
+
+
+Dependencies: ncurses
+
+
+
+Notes for devs:
+
+1. I did not make the commands that initialization ncurses a function because it is only really called once because most of the program is making uses if the library 
